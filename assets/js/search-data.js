ff164668b4c2128cbe3a0ae250213de7205a57e4
AZhou0102 <albert.zhou0412@gmail.com>
2025-08-07 08:38:36 +0000

    Deploying to gh-pages from @ AZhou0102/azhou0102.github.io@90fe52610626789b91d6d02ae0fc6bf68ab27ae2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "my research history",
+          description: "My research history",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
